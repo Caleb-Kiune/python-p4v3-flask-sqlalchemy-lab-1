9f9750af2fdb72c8c62e09d666b4eba413b7554b
Caleb <calebkiune@gmail.com>
2025-01-16 22:54:59 +0300

    adds flsk-sqlalchemy lab

--- server/app.py
+++ server/app.py
@@ -6,6 +6,9 @@ from flask_migrate import Migrate
 
 from models import db, Earthquake
 
+from sqlalchemy.orm import Session
+
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -21,6 +24,28 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+from flask import Flask, jsonify
+from models import db, Earthquake
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+db.init_app(app)
+
+@app.route('/earthquakes/<int:id>')
+def get_earthquake(id):
+    earthquake = Earthquake.query.get(id)
+    if earthquake:
+        return jsonify(earthquake.to_dict())
+    else:
+        return jsonify({"message": f"Earthquake {id} not found."}), 404
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_earthquakes_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    return jsonify({"count": len(earthquakes), "quakes": [quake.to_dict() for quake in earthquakes]})
+
+
+
 
 
 if __name__ == '__main__':
